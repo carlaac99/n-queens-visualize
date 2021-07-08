d06a76be28031175b5dd612eb9df53393f4a085d
carlaac99 <cast4730@mylaurier.ca>
2021-07-08 19:21:47 -0400

    change name

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 from nQueen import *
 app= Flask(__name__)
+if __name__ == '__main__':
+    app.run(debug=True)
 
-@app.route('/<name>')
-def index(name):
-    return "<h1>Hello{}".format(name)
+@app.route("/")
+def index():
+    #main render
+    print('index test')
+    return render_template("chessboard.html")
